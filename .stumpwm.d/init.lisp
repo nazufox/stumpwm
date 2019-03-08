a854992c9966b3a8878ef35989d055ffb5a3e60b
AsakuraNazuna <asakura@nazuna.moe>
2019-03-08 11:08:40 +0900

    わかりやすくした

--- .stumpwm.d/init.lisp
+++ .stumpwm.d/init.lisp
@@ -3,10 +3,11 @@
 (set-module-dir
  (merge-pathnames #P".stumpwm.d/stumpwm-contrib/" (user-homedir-pathname)))
 
-(defconstant +stumpwm-dir-pathname+ (merge-pathnames #P".stumpwm.d/" (user-homedir-pathname)))
+(defun load-stumpwm-conf (pathspec)
+  (load (merge-pathnames pathspec (merge-pathnames #P".stumpwm.d/" (user-homedir-pathname)))))
 
-(load (merge-pathnames "audio.lisp"     +stumpwm-dir-pathname+))
-(load (merge-pathnames "backlight.lisp" +stumpwm-dir-pathname+))
-(load (merge-pathnames "commands.lisp"  +stumpwm-dir-pathname+))
-(load (merge-pathnames "keys.lisp"      +stumpwm-dir-pathname+))
-(load (merge-pathnames "visual.lisp"    +stumpwm-dir-pathname+))
+(load-stumpwm-conf "audio.lisp")
+(load-stumpwm-conf "backlight.lisp")
+(load-stumpwm-conf "commands.lisp")
+(load-stumpwm-conf "keys.lisp")
+(load-stumpwm-conf "visual.lisp")

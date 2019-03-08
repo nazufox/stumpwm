4069143bf6281151b396f6097dd52a5dbd6b6039
AsakuraNazuna <asakura@nazuna.moe>
2019-03-08 09:30:57 +0900

    display volume when it changes

--- .stumpwm.d/commands.lisp
+++ .stumpwm.d/commands.lisp
@@ -1,17 +1,33 @@
 (in-package :stumpwm)
 
-(defcommand vol-up () ()
-  "Increase the volume"
-  (run-shell-command "pactl set-sink-volume 0 +5%"))
-(defcommand vol-down () ()
-  "Decrease the volume"
-  (run-shell-command "pactl set-sink-volume 0 -5%"))
+(defun get-volume ()
+  (ppcre:scan-to-strings "\\d+"
+                         (run-shell-command
+                          "amixer sget Master | awk '/[[0-9]+%]/ { print $(NF-1) }' | head -n 1" t)))
+(defun get-mute ()
+  (ppcre:scan-to-strings "[a-z]+"
+                         (run-shell-command
+                          "amixer sget Master | awk '/[[0-9]+%]/ { print $NF }' | head -n 1" t)))
+
 (defcommand mute-toggle () ()
   "Toggle the mute"
   (run-shell-command "pactl set-sink-mute 0 toggle"))
 (defcommand mic-toggle () ()
   "Toggle the mute of the microphone"
   (run-shell-command "pactl set-source-mute 1 toggle"))
+(defcommand vol-up () ()
+  "Increase the volume"
+  (when (string= "off" (get-mute))
+    (mute-toggle))
+  (when (< (parse-integer (get-volume)) 100)
+    (run-shell-command "pactl set-sink-volume 0 +5%"))
+  (message (get-volume)))
+(defcommand vol-down () ()
+  "Decrease the volume"
+  (when (string= "off" (get-mute))
+    (mute-toggle))
+  (run-shell-command "pactl set-sink-volume 0 -5%")
+  (message (get-volume)))
 
 (defcommand bright-up () ()
   "Increase the monitor brightness"

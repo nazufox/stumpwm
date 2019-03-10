23917b59f54317a5b63352a26054f68ae5fd0cbf
AsakuraNazuna <asakura@nazuna.moe>
2019-03-10 20:07:55 +0900

    change bar settings

--- .stumpwm.d/bar.lisp
+++ .stumpwm.d/bar.lisp
@@ -1,10 +1,11 @@
 (in-package :stumpwm)
 
-(set-fg-color "White")
-(set-bg-color "Black")
+(set-fg-color "Gray100")
+(set-bg-color "Gray20")
+(set-border-color "Gray100")
 (set-msg-border-width 1)
-(set-border-color "White")
-(setf *message-window-padding* 5)
+(setf *message-window-padding* 20)
+(setf *timeout-wait* 5)
 (setf *message-window-gravity* :center
       *input-window-gravity*   :center)
 

230a5fa890e02213f3284f9ad2c67832392fc755
AsakuraNazuna <asakura@nazuna.moe>
2019-03-12 21:03:58 +0900

    remove battery-portable module

--- .stumpwm.d/modeline.lisp
+++ .stumpwm.d/modeline.lisp
@@ -1,9 +1,5 @@
 (in-package :stumpwm)
 
-(load-module "battery-portable")
-
-(setf battery-portable:*refresh-time* 30)
-
 (setf *bar-med-color*  "^B")
 (setf *bar-hi-color*   "^B^3")
 (setf *bar-crit-color* "^B^1")
@@ -50,9 +46,9 @@
             ))
 
 (setf *mode-line-position* :bottom)
-(setf *mode-line-border-width* 4)
+(setf *mode-line-border-width* 0)
 (setf *mode-line-pad-x* 0)
-(setf *mode-line-pad-y* 3)
+(setf *mode-line-pad-y* 4)
 (setf *mode-line-background-color* "Gray20")
 (setf *mode-line-foreground-color* "Gray50")
 (setf *mode-line-border-color* "Gray30")

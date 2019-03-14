1461701660403f94d3f9e98a8fa319d8ca912cd6
AsakuraNazuna <asakura@nazuna.moe>
2019-03-14 15:42:51 +0900

    change message bar padding

--- .stumpwm.d/bar.lisp
+++ .stumpwm.d/bar.lisp
@@ -4,7 +4,8 @@
 (set-bg-color "Gray20")
 (set-border-color "Gray30")
 (set-msg-border-width 1)
-(setf *message-window-padding* 20)
+(setf *message-window-padding* 30)
+(setf *message-window-y-padding* 5)
 (setf *timeout-wait* 5)
 (setf *message-window-gravity* :center
       *input-window-gravity*   :center)

3f4942c5b145ae755579c8644b010162c63c045c
AsakuraNazuna <asakura@nazuna.moe>
2019-03-14 11:07:02 +0900

    change mode line border width and padding

--- .stumpwm.d/modeline.lisp
+++ .stumpwm.d/modeline.lisp
@@ -32,17 +32,18 @@
             (if (probe-file "/sys/class/power_supply/BAT0") ; battery
                 "  %B"
                 "")
-            "   %d  %O"
-            " "
+            "   %d  %O "
             ))
 
 (setf *mode-line-position* :bottom)
-(setf *mode-line-border-width* 4)
+(setf *mode-line-border-width* 0)
 (setf *mode-line-pad-x* 0)
-(setf *mode-line-pad-y* 3)
+(setf *mode-line-pad-y* 2)
 (setf *mode-line-background-color* "Gray20")
 (setf *mode-line-foreground-color* "Gray50")
 (setf *mode-line-border-color* "Gray30")
 (setf *mode-line-timeout* 5)
 
 (enable-mode-line (current-screen) (current-head) t)
+
+(defun restart-mode-line () (mode-line) (mode-line))

a58c0e8ec93dc534a2ffe5642506bfc51344ab71
AsakuraNazuna <asakura@nazuna.moe>
2019-03-15 22:26:07 +0900

    change message window y padding

--- .stumpwm.d/bar.lisp
+++ .stumpwm.d/bar.lisp
@@ -5,7 +5,7 @@
 (set-border-color "#f8a8bb")
 (set-msg-border-width 4)
 (setf *message-window-padding* 30)
-(setf *message-window-y-padding* 5)
+(setf *message-window-y-padding* 7)
 (setf *timeout-wait* 5)
 (setf *message-window-gravity* :center
       *input-window-gravity*   :center)

a78878085068db24ad24a09c80ee0eb979953690
AsakuraNazuna <asakura@nazuna.moe>
2019-03-16 09:24:46 +0900

    use get-universal-time

--- .stumpwm.d/modules/date-time/date-time.lisp
+++ .stumpwm.d/modules/date-time/date-time.lisp
@@ -4,13 +4,23 @@
 (add-screen-mode-line-formatter #\o 'fmt-time)
 (add-screen-mode-line-formatter #\O 'fmt-time-with-icon)
 
+(defun get-time-list ()
+  (multiple-value-list (decode-universal-time (get-universal-time))))
+
 (defun fmt-date (ml)
   (declare (ignore ml))
-  (time-format "%Y-%m-%d"))
+  (let* ((time-list (get-time-list))
+         (year  (nth 5 time-list))
+         (month (nth 4 time-list))
+         (date  (nth 3 time-list)))
+    (format nil "~d-~2,'0d-~d" year month date)))
 
 (defun fmt-time (ml)
   (declare (ignore ml))
-  (time-format "%k:%M"))
+  (let* ((time-list (get-time-list))
+         (hour   (nth 2 time-list))
+         (minute (nth 1 time-list)))
+    (format nil "~d:~d" hour minute)))
 
 (defun time-icon (hour)
   (case hour
@@ -19,5 +29,7 @@
 
 (defun fmt-time-with-icon (ml)
   (declare (ignore ml))
-  (let ((hour (parse-integer (stumpwm::time-hour-12hr))))
-    (format nil "~c ~a" (time-icon hour) (time-format "%k:%M"))))
+  (let* ((time-list (get-time-list))
+         (hour   (nth 2 time-list))
+         (minute (nth 1 time-list)))
+    (format nil "~c ~d:~d" (time-icon hour) hour minute)))

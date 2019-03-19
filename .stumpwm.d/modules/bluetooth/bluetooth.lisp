f5ce9ff9ca2d6c5815ca436d0851b7a284a3f9b1
AsakuraNazuna <asakura@nazuna.moe>
2019-03-20 00:27:33 +0900

    made bluetooth module faster.

--- .stumpwm.d/modules/bluetooth/bluetooth.lisp
+++ .stumpwm.d/modules/bluetooth/bluetooth.lisp
@@ -2,11 +2,23 @@
 
 (add-screen-mode-line-formatter #\e 'fmt-bluetooth)
 
-(defun current-state ()
-  (ppcre:scan-to-strings "on|off" (run-shell-command "bluetooth" t)))
+(defun read-type (dir)
+  (with-open-file (in (merge-pathnames #P"type" dir) :if-does-not-exist nil)
+    (when in (read in))))
+
+(defun get-devc (dev)
+  (loop :for dir :in (uiop:subdirectories #P"/sys/class/rfkill/")
+        :when (string= dev (string (read-type dir)))
+          :return (merge-pathnames #P"state" dir)))
+
+(defun get-devs (dev)
+  (let ((path (get-devc dev)))
+    (if path
+        (with-open-file (in path) (read in))
+        0)))
 
 (defun fmt-bluetooth (ml)
   (declare (ignore ml))
-  (if (string= (current-state) "on")
+  (if (= (get-devs 'bluetooth) 1)
       ""
       ""))
